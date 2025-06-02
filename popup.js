0621d751fa63234c1569b09e6a69ede5e582a58d
Diogo <sdsantosdiogo@gmail.com>
2025-06-02 17:00:51 +0100

    Atualizacao com acrescento de novos comandos novo layout

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 // Envia uma mensagem “start” à aba activa
 document.getElementById('start').addEventListener('click', () => {
+   chrome.storage.local.set({ vozAtiva: true });
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     if (tabs[0]?.id)
       chrome.tabs.sendMessage(tabs[0].id, { action: 'start-voice' });
+    
   });
 });
